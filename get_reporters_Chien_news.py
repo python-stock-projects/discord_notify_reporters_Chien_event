008f7bf18fd368ba30a9fdcfc704b0b2b64bcd9d
rrvfqo <rrvfqo@gmail.com>
2025-04-01 14:56:38 +0800

    將自動化工作流程的執行頻率從每小時調整為每四小時，並更新新聞檢查的關鍵字時間範圍

--- get_reporters_Chien_news.py
+++ get_reporters_Chien_news.py
@@ -9,43 +9,10 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-import json
-import os
 
-# 紀錄已發送的新聞
-sent_news_file = 'sent_news.json'
-if os.path.exists(sent_news_file):
-    with open(sent_news_file, 'r', encoding='utf-8') as f:
-        sent_news = set(json.load(f))
-else:
-    sent_news = set()
 
-# 紀錄上次檢查日期的文件
-last_checked_date_file = 'last_checked_date.txt'
-if os.path.exists(last_checked_date_file):
-    with open(last_checked_date_file, 'r', encoding='utf-8') as f:
-        last_checked_date = f.read().strip()
-else:
-    last_checked_date = datetime.now().strftime('%Y%m%d')
-
-def save_sent_news():
-    with open(sent_news_file, 'w', encoding='utf-8') as f:
-        json.dump(list(sent_news), f, ensure_ascii=False, indent=4)
-
-def save_last_checked_date(date):
-    with open(last_checked_date_file, 'w', encoding='utf-8') as f:
-        f.write(date)
 
 def check_news():
-    global last_checked_date
-    today = datetime.now().strftime('%Y%m%d')
-    
-    # 如果跨日，清空 sent_announcements
-    if today != last_checked_date:
-        sent_news.clear()
-        last_checked_date = today
-        save_sent_news()
-        save_last_checked_date(today)
 
     new_news = get_google_news()
     # print(f"new_news：{new_news}")
@@ -63,7 +30,7 @@ def check_news():
 
 def get_google_news():
     # 關鍵字「簡威瑟 when:1h」
-    url = f'https://news.google.com/search?q=%E7%B0%A1%E5%A8%81%E7%91%9F%20when%3A1d&hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant'
+    url = f'https://news.google.com/search?q=%E7%B0%A1%E5%A8%81%E7%91%9F%20when%3A4h&hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant'
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     }
@@ -85,17 +52,14 @@ def get_google_news():
         date_tag = item.find("time", class_="hvbAAd")
         date = date_tag.text if date_tag else "No date" # 日期
 
-        news_id = title
-        if news_id not in sent_news:
-            sent_news.add(news_id)
-            news_list.append({
-                "title": title,
-                "link": link,
-                "source": source,
-                "date": date
-            })
+
+        news_list.append({
+            "title": title,
+            "link": link,
+            "source": source,
+            "date": date
+        })
     
-    save_sent_news()
     return news_list
 
 
